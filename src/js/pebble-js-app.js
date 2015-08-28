2afc101bebf870f97e6d0aeb83c6b2519dace4d5
munazr <munaz_rahman@hotmail.com>
2015-08-27 23:00:51 -0400

    Add calls to Google Calendar API to get calendar events

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -103,7 +103,32 @@ function refresh_access_token(refresh_token, code) {
 function do_google_api() {
     use_access_token(function(access_token) {
         // Use access token to make request to Calendar API
-    });
+        var req = new XMLHttpRequest();
+        req.open("GET", "https://www.googleapis.com/calendar/v3/users/me/calendarList?access_token=" + access_token, true);
+        req.onload = function(e) {
+            if (req.readyState == 4 && req.status == 200) {
+                // Get list of Calendars https://developers.google.com/google-apps/calendar/v3/reference/calendarList/list
+                var result = JSON.parse(req.responseText);
+                // Get Calendars https://developers.google.com/google-apps/calendar/v3/reference/calendarList#resource
+                var calenders = result.items;
+                if (!calendars || calendars.length === 0) {
+                    return;
+                }
+                req = new XMLHttpRequest():
+                req.open("GET", "https://www.googleapis.com/calendar/v3/calendars/" + calendars[0].id + "/events?maxResults=10&orderBy=startTime", true)
+                req.onload = function(e) {
+                    if (req.readyState = 4 && req.status == 200) {
+                        // Get list of events https://developers.google.com/google-apps/calendar/v3/reference/events/list
+                        result = JSON.parse(req.responseText);
+                        // Get events https://developers.google.com/google-apps/calendar/v3/reference/events#resource
+                        var events = result.items;
+                        // Do stuff with events here
+                    }
+                }
+                req.send();
+            }
+        };
+        req.send();
 }
 
 // When you click on Settings in Pebble's phone app. Go to the configuration.html page.
